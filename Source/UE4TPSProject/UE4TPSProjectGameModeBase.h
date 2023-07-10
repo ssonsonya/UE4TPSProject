6587bcfe1075c57eef83761458213c50e1eaed73
Sonya <hyonseonkim@gmail.com>
2023-07-11 08:26:07 +0900

    Initial commit

---
+++ Source/UE4TPSProject/UE4TPSProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4TPSProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4TPSPROJECT_API AUE4TPSProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
